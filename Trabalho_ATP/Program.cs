e8b197b088f2dc2771cc9ee7747a8d85d5aae5c2
Ana Clara <acccarneiro@sga.pucminas.br>
2025-11-11 20:47:01 -0300

    Adicionar arquivos de projeto.

---
+++ Trabalho_ATP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Trabalho_ATP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //criando repositório
+        }
+    }
+}
