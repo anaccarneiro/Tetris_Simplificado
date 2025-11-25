9ebe249184cbc47af75930be51c876e2fec4a328
Ana Clara <acccarneiro@sga.pucminas.br>
2025-11-25 16:58:01 -0300

    Implementa classes Peca e Tabuleiro
    - Criada classe Peca
    - Criada classe Tabuleiro

--- Trabalho_ATP/Tabuleiro.cs
+++ Trabalho_ATP/Tabuleiro.cs
@@ -4,5 +4,47 @@ namespace Trabalho_ATP
 {
     internal class Tabuleiro
     {
+        public char[,] tabuleiro;
+
+        public Tabuleiro()
+        {
+            tabuleiro = new char[20, 10];
+
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                    tabuleiro[i, j] = '.';
+            }
+        }
+
+        public void Renderizar()
+        {
+            Console.Clear();
+
+            for (int i = 0; i < 20; i++)
+            {
+
+
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(tabuleiro[i, j]);
+                    int posY = j;
+                }
+                Console.WriteLine();
+            }
+        }
+        public void Limpar(int posX, int posY, int[,] forma)
+        {
+            for (int i = 0; i < forma.GetLength(0); i++)
+            {
+                for (int j = 0; j < forma.GetLength(1); j++)
+                {
+                    if (forma[i, j] == 1)
+                    {
+                        tabuleiro[posY + i, posX + j] = '.';
+                    }
+                }
+            }
+        }
     }
 }
