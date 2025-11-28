cc434db302ef5f021687a381c0d83f11271641d6
Samira0428 <samira.fraga@sga.pucminas.br>
2025-11-28 16:03:37 -0300

    Fase 1) Criando a CLASSE JOGADOR com nome e pontuação

--- Trabalho_ATP/Jogador.cs
+++ Trabalho_ATP/Jogador.cs
@@ -1,8 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Trabalho_ATP
+namespace ConsoleApp1
 {
-    internal class Jogador
+    public class Jogador
     {
+        public string nome { get; set; }
+        public int pontuacaoFinal { get; set; }
+
+        public Jogador(string nome)
+        {
+            this.nome = nome;
+            this.pontuacaoFinal = 0;
+        }
+
+        public void AdicionarPontos(int pontos)
+        {
+            pontuacaoFinal += pontos;
+        }
+
+        public void Salvar(string caminho)
+        {
+            using (StreamWriter sw = new StreamWriter(caminho, true))
+            {
+                sw.WriteLine($"{nome}:{pontuacaoFinal}");
+            }
+        }
     }
 }
