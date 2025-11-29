3f088c0f55d020410fa9df56ad832e4fb2a48883
Samira0428 <samira.fraga@sga.pucminas.br>
2025-11-28 22:50:33 -0300

    Criando a CLASSE JOGO e o esqueleto do método  iniciar

--- Trabalho_ATP/Jogo.cs
+++ Trabalho_ATP/Jogo.cs
@@ -1,8 +1,45 @@
-﻿using System;
+﻿using ConsoleApp1;
+using System;
 
 namespace Trabalho_ATP
 {
-    internal class Jogo
+    public class Jogo
     {
+        private Jogador jogador;
+        private int[,] tabuleiro;
+        private bool jogoAtivo;
+
+        public Jogo(Jogador jogador)
+        {
+            this.jogador = jogador;
+            tabuleiro = new int[20, 10];
+            jogoAtivo = true;
+        }
+
+        public void Iniciar()
+        {
+            while (jogoAtivo)
+            {
+                Console.Clear();
+                ExibirTabuleiro();
+                LerEntrada();
+                AtualizarJogo();
+            }
+
+            jogador.Salvar("pontuacoes.txt");
+        }
+
+        private void ExibirTabuleiro()
+        {
+        }
+
+        private void LerEntrada()
+        {
+        }
+
+        private void AtualizarJogo()
+        {
+        }
+
     }
 }
